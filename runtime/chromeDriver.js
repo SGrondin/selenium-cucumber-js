b1dd0a3b1e9b50cebfe9df12ca5899a673d32b3e
Simon Grondin <github@simongrondin.name>
2018-10-29 10:21:20 -0500

    Don't maximize chrome

--- runtime/chromeDriver.js
+++ runtime/chromeDriver.js
@@ -14,7 +14,7 @@ module.exports = function() {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-            args: ['start-maximized', 'disable-extensions']
+            args: ['disable-extensions']
         },
         path: chromedriver.path
     }).build();
